549046d037feda1b73e9e1abdb82e60c5b298b0a
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2022-02-22 11:08:05 +0530

    Create static2.java

---
+++ static2.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Main.method();
+	}
+		static void method(){
+		    System.out.println("Static block");
+		}
+}
