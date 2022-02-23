a9bc566d551c60a327fc9943270caf4fbea84c2e
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2022-02-23 11:44:05 +0530

    Create ExplicitClock.java

---
+++ ExplicitClock.java
@@ -0,0 +1,9 @@
+#jvm moves in clock wise direction show it returns -126 order are in clock wise (0,1,2,....,127,-128,-127,-126)
+public class Main
+{
+	public static void main(String[] args) {
+		int a=130;
+		byte x=(byte)a;
+	    System.out.println(x);
+	}
+}
